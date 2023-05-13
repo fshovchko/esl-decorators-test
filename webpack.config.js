3791ffe23250f5ad7e9c5c2e798c7739e4b17c66
Feoktist Shovchko <fshovchko@exadel.com>
2023-05-13 20:01:19 +0300

    chore: add confguration files

---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+  entry: [path.resolve(__dirname, './src/index.js'), path.resolve(__dirname, './src/index.css')],
+
+  mode: 'development',
+
+  devServer: {
+    static: {
+      directory: path.join(__dirname, './dist'),
+    },
+    open: true,
+    compress: true,
+    port: 9000,
+  },
+
+  output: {
+    filename: 'bundle.[contenthash].js',
+    path: path.resolve(__dirname, './dist')
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './src/index.html'),
+      minify: true
+    }),
+  ],
+
+  module: {
+    rules: [{
+      test: /\.js$/,
+      exclude: /node_modules/,
+      use: ['babel-loader']
+    }, {
+      test: /\.css$/,
+      use: ['style-loader', 'css-loader']
+    }]
+  }
+}
